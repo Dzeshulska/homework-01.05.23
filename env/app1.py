f37d95a2e0a9e3998537a096d220870c75a3ed76
Alla_Dseshulska <dzeshulska@gmail.com>
2023-04-26 21:50:11 +0300

    added input

--- env/app1.py
+++ env/app1.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 
+from selenium.webdriver.common.by import By
+
 from selenium.webdriver.common.keys import Keys
 
 import time
@@ -12,18 +14,13 @@ driver.implicitly_wait(0.5)
 
 #launch URL
 
-driver.get(from selenium.webdriver.common.keys import Keys
-
-import time
-
-#set chromodriver.exe path
-
-driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
-
-driver.implicitly_wait(0.5)
-
-#launch URL
-
-driver.get("https://www.rozetka.ua in Mozila Firebox")
+driver.get("https://www.rozetka.ua")
 
 time.sleep(10)
+
+text_input = driver.find_element(By.ID, "textInput")
+ActionChains(driver)\
+      .send_keys_to_element(text_input, "samsung")\
+      .perform()
+
+time.sleep(20)
