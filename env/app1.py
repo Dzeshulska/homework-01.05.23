e90d2aea0a4c53e5c5e575fd8b977fdaff1d74a3
Alla_Dseshulska <dzeshulska@gmail.com>
2023-04-26 20:51:14 +0300

    addede changes more

---
+++ env/app1.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+
+from selenium.webdriver.common.keys import Keys
+
+import time
+
+#set chromodriver.exe path
+
+driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
+
+driver.implicitly_wait(0.5)
+
+#launch URL
+
+driver.get(from selenium.webdriver.common.keys import Keys
+
+import time
+
+#set chromodriver.exe path
+
+driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
+
+driver.implicitly_wait(0.5)
+
+#launch URL
+
+driver.get("https://www.rozetka.ua in Mozila Firebox")
+
+time.sleep(10)
